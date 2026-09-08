821d2c004ae3b1083225dcfadf4d33343615cc6b
sphoffman <Shoffman73@yahoo.com>
2026-09-07 21:57:31 -0400

    Assert recovery port remains in edge policy

--- tests/test_provisioning_design.py
+++ tests/test_provisioning_design.py
@@ -49,6 +49,8 @@ def test_template_excludes_bootstrap_and_device_write_material():
     assert "vlan members all" in template
     assert "{{management_prefix}}" in template
     assert "{{management_ip}}/24" not in template
+    assert 'set interfaces interface-range edge_ports member "ge-[0-9]/0/[2-47]"' in template
+    assert "set interfaces {{recovery_interface}} unit 0 family ethernet-switching vlan members {{temporary_recovery_vlan_name}}" in template
 
 
 def test_contract_keeps_bootstrap_variables_outside_template():
@@ -58,8 +60,10 @@ def test_contract_keeps_bootstrap_variables_outside_template():
     assert "management_ip" in required
     assert "management_prefix" in required
     assert "voice_vlan" in required
+    assert "recovery_interface" in required
     assert "fxp0_management_ip" in bootstrap
     assert not required.intersection(bootstrap)
+    assert "TEMPORARY_RECOVERY_PORT_OVERLAY" in contract["phase_contract"]["pre_stage"]["include"]
     assert contract["safety"]["credentials_allowed"] is False
 
 
