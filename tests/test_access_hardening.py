e5247d0594014fe91088160d6b11e8e380cfbd36
sphoffman <Shoffman73@yahoo.com>
2026-09-09 15:40:52 -0400

    Test template edge range, broad voice preservation, and stale VLAN pruning

--- tests/test_access_hardening.py
+++ tests/test_access_hardening.py
@@ -3,17 +3,21 @@ import pytest
 from ex_migration_provisioner.access_hardening import (
     classify_final_ports,
     current_port_states,
+    edge_interfaces,
+    expand_edge_range,
+    final_production_vlan_names,
     hardening_statements,
+    stale_vlan_cleanup,
     validate_final_hardening,
     validate_pre_hardening_config,
 )
 
 
-def _mac(mac, interface):
+def _mac(mac, interface, vlan_name="default", vlan_id=3998):
     return "\n".join([
         "MAC address: %s" % mac,
         "Routing instance: default-switch",
-        "VLAN name: default, VLAN ID: 3998",
+        "VLAN name: %s, VLAN ID: %s" % (vlan_name, vlan_id),
         "Learning interface: %s.0" % interface,
         "Layer 2 flags: 0x1",
         "",
@@ -29,19 +33,75 @@ def _plan():
     }
 
 
+def test_edge_range_expands_junos_numeric_ranges_from_live_template_policy():
+    assert expand_edge_range("ge-[0-9]/0/[2-47]", [0]) == [
+        "ge-0/0/%d" % port for port in range(2, 48)
+    ]
+    values = expand_edge_range("ge-[0-9]/0/[0-47]", [0, 1])
+    assert "ge-0/0/0" in values
+    assert "ge-1/0/47" in values
+    assert len(values) == 96
+
+
+def test_edge_interfaces_uses_live_template_range_and_rejects_uplink_overlap():
+    config = 'set interfaces interface-range edge_ports member "ge-[0-9]/0/[2-47]"\n'
+    values = edge_interfaces(config, [0], ["ge-0/0/0", "ge-0/0/1"])
+    assert values[0] == "ge-0/0/2"
+    assert values[-1] == "ge-0/0/47"
+    assert len(values) == 46
+
+    prod = 'set interfaces interface-range edge_ports member "ge-[0-9]/0/[0-47]"\n'
+    values = edge_interfaces(prod, [0], ["et-0/0/48", "et-0/0/49"])
+    assert values[0] == "ge-0/0/0"
+    with pytest.raises(Exception):
+        edge_interfaces(prod, [0], ["ge-0/0/0"])
+
+
+def test_final_production_vlans_follow_qfx_lineage_plus_management():
+    configured = [
+        {"name": "v100", "vlan_id": 100, "classification": "data"},
+        {"name": "v163", "vlan_id": 163, "classification": "management"},
+        {"name": "v200", "vlan_id": 200, "classification": "data"},
+        {"name": "v300", "vlan_id": 300, "classification": "data"},
+        {"name": "voip", "vlan_id": 1111, "classification": "voice"},
+    ]
+    assert final_production_vlan_names(configured, 163, [100, 200, 1111]) == [
+        "v100", "v163", "v200", "voip"
+    ]
+
+
 def test_classification_keeps_completed_and_disables_proven_unused():
     states = {
         "ge-0/0/2": {"interface": "ge-0/0/2", "state": "LINK_DOWN", "admin_status": "up", "oper_status": "down", "dynamic_mac_present": False},
         "ge-0/0/10": {"interface": "ge-0/0/10", "state": "LINK_DOWN", "admin_status": "up", "oper_status": "down", "dynamic_mac_present": False},
         "ge-0/0/11": {"interface": "ge-0/0/11", "state": "ADMIN_DOWN", "admin_status": "down", "oper_status": "down", "dynamic_mac_present": False},
-        "ge-0/0/47": {"interface": "ge-0/0/47", "state": "UP_SILENT", "admin_status": "up", "oper_status": "up", "dynamic_mac_present": False},
+        "ge-0/0/47": {"interface": "ge-0/0/47", "state": "LINK_DOWN", "admin_status": "up", "oper_status": "down", "dynamic_mac_present": False},
     }
     completed = {"ge-0/0/2": {"old_interface": "ge-0/0/2", "new_interface": "ge-0/0/2"}}
     value = classify_final_ports(_plan(), completed, states, "ge-0/0/47")
     assert value["result"] == "PASS"
     assert [row["interface"] for row in value["used"]] == ["ge-0/0/2"]
     assert [row["interface"] for row in value["unused"]] == ["ge-0/0/10", "ge-0/0/11", "ge-0/0/47"]
-    assert next(row for row in value["unused"] if row["interface"] == "ge-0/0/47")["reason"] == "RECOVERY_PORT_RETIRED_AT_CLEANUP"
+    assert next(row for row in value["unused"] if row["interface"] == "ge-0/0/47")["reason"] == "RECOVERY_PORT_PROVEN_INACTIVE_AT_CLEANUP"
+
+
+def test_live_or_silent_recovery_port_blocks_cleanup():
+    for state, mac, reason in (
+        ("ACTIVE_MAC", True, "RECOVERY_PORT_CURRENTLY_ACTIVE"),
+        ("UP_SILENT", False, "RECOVERY_PORT_UP_SILENT"),
+    ):
+        states = {
+            "ge-0/0/47": {
+                "interface": "ge-0/0/47",
+                "state": state,
+                "admin_status": "up",
+                "oper_status": "up",
+                "dynamic_mac_present": mac,
+            }
+        }
+        value = classify_final_ports(_plan(), {}, states, "ge-0/0/47")
+        assert value["result"] == "FAIL"
+        assert value["blockers"][0]["reason"] == reason
 
 
 def test_completed_admin_down_port_blocks_cleanup():
@@ -79,6 +139,7 @@ def test_current_port_state_detects_mac_and_silent():
 
 def _pre_config():
     return "\n".join([
+        'set interfaces interface-range edge_ports member "ge-[0-9]/0/[2-47]"',
         "set interfaces ae0 unit 0 family ethernet-switching interface-mode trunk",
         "set interfaces ae0 unit 0 family ethernet-switching vlan members all",
         "set interfaces ge-0/0/47 unit 0 family ethernet-switching vlan members TEMP-RECOVERY",
@@ -90,6 +151,7 @@ def test_pre_hardening_config_accepts_expected_template_state():
     classification = {"unused": [{"interface": "ge-0/0/10"}, {"interface": "ge-0/0/47"}]}
     value = validate_pre_hardening_config(_pre_config(), classification, "ge-0/0/47", "TEMP-RECOVERY", "voip")
     assert value["result"] == "PASS"
+    assert value["edge_ports_expression"] == "ge-[0-9]/0/[2-47]"
 
 
 def test_pre_hardening_blocks_unexpected_vlan_on_unused_port():
@@ -100,20 +162,54 @@ def test_pre_hardening_blocks_unexpected_vlan_on_unused_port():
     assert value["unexpected_unused_port_memberships"][0]["unexpected"] == ["v100"]
 
 
-def test_hardening_replaces_all_trunk_and_narrows_voice_policy():
+def test_stale_vlan_cleanup_deletes_only_proven_unused_data_vlan():
+    configured = [
+        {"name": "v100", "vlan_id": 100, "classification": "data"},
+        {"name": "v163", "vlan_id": 163, "classification": "management"},
+        {"name": "v300", "vlan_id": 300, "classification": "data"},
+    ]
+    config = "\n".join([
+        "set vlans v100 vlan-id 100",
+        "set vlans v163 vlan-id 163",
+        "set vlans v300 vlan-id 300",
+        "set vlans v300 description legacy",
+        "set vlans v300 forwarding-options dhcp-security group DHCP_TRUST interface ae0.0",
+    ])
+    value = stale_vlan_cleanup(config, "", configured, [100, 163])
+    assert [row["name"] for row in value["delete"]] == ["v300"]
+
+    referenced = config + "\nset interfaces ge-0/0/20 unit 0 family ethernet-switching vlan members v300\n"
+    value = stale_vlan_cleanup(referenced, "", configured, [100, 163])
+    assert value["delete"] == []
+    assert value["preserve"][0]["reason"] == "EXTERNAL_CONFIGURATION_REFERENCE"
+
+    dynamic = _mac("02:00:00:00:03:00", "ge-0/0/20", "v300", 300)
+    value = stale_vlan_cleanup(config, dynamic, configured, [100, 163])
+    assert value["delete"] == []
+    assert value["preserve"][0]["reason"] == "DYNAMIC_MAC_PRESENT"
+
+
+def test_hardening_replaces_all_preserves_voice_and_prunes_stale_vlan():
+    config = _pre_config() + "\n" + "\n".join([
+        "set vlans v300 vlan-id 300",
+        "set vlans v300 description legacy",
+        "set vlans v300 forwarding-options dhcp-security group DHCP_TRUST interface ae0.0",
+    ])
     statements = hardening_statements(
-        _pre_config(),
+        config,
         ["ge-0/0/10", "ge-0/0/47"],
         ["ge-0/0/2", "ge-0/0/3"],
         ["v100", "v163", "v200", "voip"],
         "voip",
         "TEMP-RECOVERY",
+        stale_vlan_names=["v300"],
     )
     assert statements[0] == "delete interfaces ae0 unit 0 family ethernet-switching vlan members all"
     assert "set interfaces ge-0/0/10 disable" in statements
     assert "set interfaces ge-0/0/10 unit 0 family ethernet-switching vlan members default" in statements
-    assert "delete switch-options voip interface edge_ports vlan voip" in statements
-    assert "set switch-options voip interface ge-0/0/2 vlan voip" in statements
+    assert not any("switch-options voip" in statement for statement in statements)
+    assert "delete vlans v300 vlan-id 300" in statements
+    assert "delete vlans v300 description legacy" in statements
 
 
 def test_hardening_does_not_repeat_preexisting_unused_state():
@@ -123,17 +219,27 @@ def test_hardening_does_not_repeat_preexisting_unused_state():
     assert "set interfaces ge-0/0/10 unit 0 family ethernet-switching vlan members default" not in statements
 
 
-def test_final_hardening_validation():
+def test_final_hardening_validation_preserves_range_and_broad_voice_policy():
     config = "\n".join([
+        'set interfaces interface-range edge_ports member "ge-[0-9]/0/[2-47]"',
         "set interfaces ae0 unit 0 family ethernet-switching interface-mode trunk",
         "set interfaces ae0 unit 0 family ethernet-switching vlan members v100",
         "set interfaces ae0 unit 0 family ethernet-switching vlan members v163",
         "set interfaces ae0 unit 0 family ethernet-switching vlan members voip",
         "set interfaces ge-0/0/10 disable",
         "set interfaces ge-0/0/10 unit 0 family ethernet-switching vlan members default",
-        "set switch-options voip interface ge-0/0/2 vlan voip",
+        "set switch-options voip interface edge_ports vlan voip",
     ])
-    value = validate_final_hardening(config, {}, ["ge-0/0/2"], ["ge-0/0/10"], ["v100", "v163", "voip"], "voip")
+    value = validate_final_hardening(
+        config,
+        {},
+        ["ge-0/0/2"],
+        ["ge-0/0/10"],
+        ["v100", "v163", "voip"],
+        "voip",
+        deleted_vlan_names=["v300"],
+        expected_edge_expression="ge-[0-9]/0/[2-47]",
+    )
     assert value["result"] == "PASS"
 
 
